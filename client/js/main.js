2f71a120dc60a88f4e73ffbddb8b02b145f78912
Daniel <ddefisher@gmail.com>
2016-02-26 01:34:59 -0500

    keypad to scroll

--- client/js/main.js
+++ client/js/main.js
@@ -1,6 +1,35 @@
+var ele_ids = ['about_me','projects','resume'];
+var index = 0;
+
 function scroll_id(eleID) {
+
    var e = document.getElementById(eleID);
    if (!!e && e.scrollIntoView) {
        e.scrollIntoView();
    }
 }
+//scroll page up and down with key press
+document.onkeypress = function (e) {
+
+    e = e || window.event;
+    //key press was right or down
+    if(e.keyCode === 39 || e.keyCode === 40) {
+
+        if(index < 2) {
+            index++;
+            scroll_id(ele_ids[index]);
+        }
+
+        return;
+    }
+    //key press was left or up
+    if(e.keyCode === 38 || e.keyCode === 37) {
+
+        if(index > 0) {
+            index--;
+            scroll_id(ele_ids[index]);
+        }
+
+        return;
+    }
+};
